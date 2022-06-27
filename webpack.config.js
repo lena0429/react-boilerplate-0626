7115ad3cc9f81ddc3691327137c94146d62dff35
Lena Hou <lenahou2013@gmail.com>
2022-06-27 13:29:04 -0400

    created webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 module.exports = {
-    entry: "",
-    output: {},
-    devServer: {},
-    devtool: debug ? "cheap-module-eval-source-map" : false,
-    resolve: {},
-    module: {
-      rules: []
+  module:{
+    rules:[
+      {
+        test:/\.m?js$/,
+        exclude:/(node_modules|bower_components)/,
+        use:{
+          loader:"babel-loader",
+          options:{
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+    ],
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: "source-map",
+  mode:"production",
+  plugins:[
+    new HtmlWebpackPlugin(
+      {
+        filename:'index.html',
+        template:"./src/index.html",
+      }
+    )
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
     },
-    plugins: [],
-    optimization: {}
-  };
-
-
-  
+    compress: true,
+    port: 3000,
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
+};
