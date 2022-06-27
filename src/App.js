f0efe45959553518f08b79f35801ec0caeefbd3e
Lena Hou <lenahou2013@gmail.com>
2022-06-27 13:01:11 -0400

    set up files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import "./App.css";
+class App extends Component {
+render() {
+    return (
+      <div className="App">
+        <header className="App-header">WELCOME TO NOTHING 🤗</header>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: "",
+    output: {},
+    devServer: {},
+    devtool: debug ? "cheap-module-eval-source-map" : false,
+    resolve: {},
+    module: {
+      rules: []
+    },
+    plugins: [],
+    optimization: {}
+  };
+
+
+  
